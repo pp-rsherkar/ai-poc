6a4234c35edf5846dac683a3f12e861cc8acfb71
psevak <psevak@pulsepoint.com>
2025-02-17 13:35:44 +0530

    Code for navigation to Life, HCP and Studio added in Navigation page file.

--- src/main/java/pages/Navigation.java
+++ src/main/java/pages/Navigation.java
@@ -2,18 +2,27 @@ package pages;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 
 public class Navigation {
     public final Locator USERNAME;
     private final Locator PASSWORD;
     private final Locator LOGIN_BUTTON;
     private final Page page;
+    private final Locator LIFE;
+    private final Locator SIGNAL;
+    private final Locator STUDIO;
+    private final Locator SUB_MENU;
 
     public Navigation(Page page) {
         this.page = page;
         this.USERNAME = page.locator("#UserName");
         this.PASSWORD = page.locator("#Password");
         this.LOGIN_BUTTON = page.locator(".loginLabel");
+        this.LIFE = page.getByText("Life");
+        this.SIGNAL = page.getByText("Signal");
+        this.STUDIO = page.getByText("Studio");
+        this.SUB_MENU = page.getByRole(AriaRole.IMG, new Page.GetByRoleOptions().setName("menu"));
     }
 
     public void navigateToUrl(String url) {
@@ -39,5 +48,20 @@ public class Navigation {
     public String verifyProfilePage() {
         page.waitForLoadState();
         return this.page.title();
+    }
+
+    public void navigateToLife() {
+        LIFE.click();
+    }
+
+    public void navigateToHCP() {
+        SIGNAL.click();
+    }
+
+    public void navigateToStudio() {
+        page.waitForLoadState();
+        SUB_MENU.click();
+        page.waitForLoadState();
+        STUDIO.click();
     }
 }
