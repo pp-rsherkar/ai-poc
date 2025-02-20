59528902632b173e481db9fcae17d9a3a6e8bf38
psevak <psevak@pulsepoint.com>
2025-02-20 13:20:21 +0530

    HCP Explorer flow added

--- src/main/java/pages/studio/Workspaces.java
+++ src/main/java/pages/studio/Workspaces.java
@@ -1,11 +1,48 @@
 package pages.studio;
 
+import com.microsoft.playwright.FrameLocator;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.options.WaitForSelectorState;
+
+import java.util.regex.Pattern;
 
 public class Workspaces {
     private final Page page;
+    private final Locator WORKSPACE_CONTENT;
+    private final Locator CREATE_WORKSPACE;
+    private final Locator HCP_EXPLORER;
+    private final Locator HCP_EXPANSION;
 
     public Workspaces(Page page) {
         this.page = page;
+        this.WORKSPACE_CONTENT = page.locator("#extension-root > div > div:nth-child(1) > div > div > div > div > div.Box-sc-5738oh-0.styles__StyledTableContainer-sc-ga32ay-1.czcvII.fTjMUM > table");
+        this.CREATE_WORKSPACE = page.locator("iframe[title=\"overview\"]").contentFrame().locator("iframe").contentFrame().getByRole(AriaRole.BUTTON, new FrameLocator.GetByRoleOptions().setName("Create New Workspace"));
+        this.HCP_EXPLORER = page.locator("iframe[title=\"overview\"]").contentFrame().locator("iframe").contentFrame().getByText("HCP Explorer");
+        this.HCP_EXPANSION = page.locator("iframe[title=\"overview\"]").contentFrame().locator("iframe").contentFrame().getByText("HCP Audience Expansion");
+    }
+
+    public String studioDashboard() {
+        page.waitForLoadState();
+        return this.page.title();
+    }
+
+    public void createWorkspace() {
+        CREATE_WORKSPACE.click();
+    }
+
+    public String verifyHCPExplorer() {
+        page.waitForLoadState();
+        return HCP_EXPLORER.innerText();
+    }
+
+    public String verifyHCPAudienceExpansion() {
+        return HCP_EXPANSION.innerText();
+    }
+
+    public void clickHCPExplorerWorkspace() {
+        HCP_EXPLORER.click();
     }
 }
--- src/test/java/stepdefinitions/StudioSteps.java
+++ src/test/java/stepdefinitions/StudioSteps.java
@@ -2,15 +2,75 @@ package stepdefinitions;
 
 import factory.DriverFactory;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import pages.*;
-import utils.WebActions;
+import pages.studio.ExplorerWorkspace;
+import pages.studio.Workspaces;
 
 import java.util.UUID;
 
 public class StudioSteps {
+    static String workspaceNameRandom;
+    static String filterName;
+    static String filterOption;
+    Workspaces workspaces = new Workspaces(DriverFactory.getPage());
+    ExplorerWorkspace explorerWorkspace = new ExplorerWorkspace(DriverFactory.getPage());
 
+    @When("User clicks on Create New Workspace")
+    public void user_clicks_on_create_new_workspace() {
+        Assert.assertEquals("", "Genome Studio", workspaces.studioDashboard());
+        workspaces.createWorkspace();
+    }
+
+    @Then("User sees the types of workspaces they have permissions for")
+    public void user_sees_the_types_of_workspaces_they_have_permissions_for() {
+        Assert.assertEquals("HCP Explorer", workspaces.verifyHCPExplorer());
+        System.out.println(workspaces.verifyHCPExplorer());
+        Assert.assertEquals("HCP Audience Expansion", workspaces.verifyHCPAudienceExpansion());
+        System.out.println(workspaces.verifyHCPAudienceExpansion());
+    }
+
+    @And("User clicks on HCP Explorer workspace")
+    public void user_clicks_on_hcp_explorer_workspace() {
+        workspaces.clickHCPExplorerWorkspace();
+    }
+
+    @Then("User adds the workspace name as {string} and selects the advertiser {string}")
+    public void user_adds_the_workspace_name_and_selects_the_advertiser(String workspaceName, String advertiser) {
+        workspaceNameRandom = workspaceName + '_' + UUID.randomUUID().toString().substring(0, 10);
+        explorerWorkspace.enterWorkspaceName(workspaceNameRandom);
+        explorerWorkspace.selectAdvertiser(advertiser);
+    }
+
+    @Then("User applies the {string} filter and selects {string} option")
+    public void user_applies_the_filters_as_gender_and_age(String filter, String option) {
+        filterName = filter;
+        filterOption = option;
+        explorerWorkspace.selectFilter(filterName, filterOption);
+    }
+
+    @Then("User clicks on Ok and closes the filter popup")
+    public void user_clicks_on_ok_and_closes_the_filter_popup() {
+        explorerWorkspace.applyFilter();
+    }
+
+    @Then("Verify that the applied filters are displayed correctly")
+    public void verify_that_the_applied_filters_are_displayed_correctly() {
+        Assert.assertEquals(filterName, explorerWorkspace.verifySelectedFilter());
+        System.out.println(explorerWorkspace.verifySelectedFilter());
+        Assert.assertEquals(filterOption, explorerWorkspace.verifySelectedOption());
+        System.out.println(explorerWorkspace.verifySelectedOption());
+    }
+
+    @Then("User saves the workspace")
+    public void user_saves_the_workspace() {
+        explorerWorkspace.saveExplorerWorkspace();
+    }
+
+    @Then("Verify the HCP Explorer Workspace is saved")
+    public void verify_the_hcp_explorer_workspace_is_saved() {
+        assert explorerWorkspace.workspaceSuccess().contains("Workspace saved");
+        System.out.println(explorerWorkspace.workspaceSuccess());
+    }
 }
