e2a5d3c3b8c845d2ed2ef7e6e7a3ba12ff51a5db
psevak <psevak@pulsepoint.com>
2025-08-19 16:46:20 +0530

    E2E - Conversion pixel creation and targeting at tactic level

--- src/test/java/stepdefinitions/LifeSteps.java
+++ src/test/java/stepdefinitions/LifeSteps.java
@@ -31,6 +31,7 @@ public class LifeSteps {
     static String templateNameRandom;
     static String dimensionName;
     static String metricName;
+    static String newPixelName;
     List<Object> keyType = new ArrayList<>();
     List<Object> keyValues = new ArrayList<>();
     Map<String, Map<String, String>> keyValueMap = new LinkedHashMap<>();
@@ -49,6 +50,8 @@ public class LifeSteps {
     CampaignDashboard campaignDashboard = new CampaignDashboard(DriverFactory.getPage());
     TargetingTemplate targetingTemplate = new TargetingTemplate(DriverFactory.getPage());
     NPIAttributesList npiAttributesList = new NPIAttributesList(DriverFactory.getPage());
+    Pixels pixels = new Pixels(DriverFactory.getPage());
+    ConversionPixel conversionPixel = new ConversionPixel(DriverFactory.getPage());
     Constants constants = new Constants();
     String timestamp = CommonUtils.timeStampCalculation();
     boolean flag = false;
@@ -1096,6 +1099,62 @@ public class LifeSteps {
     @Then("Verify the template created are saved")
     public void verifyTheTemplateCreatedAreSaved() {
         Assert.assertFalse("Unable to save targeting templates", keyValueMap.isEmpty());
+    }
+
+    @And("User navigates to Pixels page")
+    public void userNavigatesToPixelsPage() {
+        navigation.clickSubMenu();
+        pixels.clickPixelsMenuItem();
+    }
+
+    @When("User clicks on Add Pixel button")
+    public void userClicksOnAddPixelButton() {
+        pixels.clickAddPixelButton();
+    }
+
+    @Then("Verify the Create New Pixel panel and types of Pixel")
+    public void verifyCreateNewPixelPanelAndTypesOfPixel() {
+        Assert.assertEquals("CREATE NEW PIXEL", pixels.verifyCreateNewPixelLabel().toUpperCase());
+        Assert.assertEquals("RETARGETING PIXEL", pixels.verifyRetargetingPixel().toUpperCase());
+        Assert.assertEquals("SMART PIXEL", pixels.verifySmartPixel().toUpperCase());
+        Assert.assertEquals("CONVERSION PIXEL", pixels.verifyConversionPixel().toUpperCase());
+    }
+
+    @And("User selects the {string} type")
+    public void userSelectsThePixelType(String pixelType) {
+        pixels.selectPixelType(pixelType);
+    }
+
+    @And("User enters the pixel details as {string} {string} {string} {string}")
+    public void userEntersPixelDetails(String pixelName, String advertiser, String conversionPixelScope, String conversionPixelType) {
+        newPixelName = pixelName + '_' + timestamp;
+        conversionPixel.enterPixelName(newPixelName);
+        conversionPixel.selectAdvertiser(advertiser);
+        conversionPixel.selectConversionPixelScope(conversionPixelScope);
+        conversionPixel.selectConversionPixelType(conversionPixelType);
+    }
+
+    @And("User saves the pixel")
+    public void userSavesThePixel() {
+        pixels.savePixel();
+    }
+
+    @Then("Verify the pixel is saved successfully and displayed in the pixel list")
+    public void verifyPixelIsSavedSuccessfullyAndDisplayedInPixelList() {
+        assert pixels.verifySaveSuccess().contains("Success!");
+        pixels.searchSavedPixel(newPixelName);
+        Assert.assertEquals(newPixelName, pixels.verifyCreatedPixel(newPixelName));
+    }
+
+    @When("User selects {string} as rule type and selects the created pixel")
+    public void userSelectsRuleTypeAndSelectsCreatedPixelAndSavesSettings(String ruleType) {
+        tacticSettings.selectRuleType(ruleType, newPixelName);
+    }
+
+    @Then("Verify the selected targeting rule {string}")
+    public void verifyTheSelectedTargetingRules(String ruleType) {
+        Assert.assertEquals(ruleType, tacticSettings.verifyRuleType());
+        Assert.assertEquals(newPixelName, tacticSettings.verifyRuleOption());
     }
 
 }
