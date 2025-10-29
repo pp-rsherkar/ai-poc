0a4cf84654e16a3fec8f443115e489764fefced6
pp-vpandey <vpandey@pulsepoint.com>
2025-10-29 12:53:15 +0530

    Reverted change in CampaignDashbaord.java file

--- src/main/java/pages/life/CampaignDashboard.java
+++ src/main/java/pages/life/CampaignDashboard.java
@@ -380,7 +380,7 @@ public class CampaignDashboard {
         return true;
     }
 
-    public void navigateToLineItemDetails(String campaignName){
+    public void navigateToLineItemDetails(String campaignID){
         LINE_ITEM_NAME.click();
         LINE_ITEM_PAGE_TITLE.waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE));
     }
