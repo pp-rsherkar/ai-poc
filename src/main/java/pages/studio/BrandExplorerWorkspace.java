99687f0b8ed6facfe87af22c5aee27cf7289a75c
Poulomi <pmitra@pulsepoint.com>
2026-06-19 16:41:14 +0530

    Added locators and page methods

--- src/main/java/pages/studio/BrandExplorerWorkspace.java
+++ src/main/java/pages/studio/BrandExplorerWorkspace.java
@@ -3,6 +3,8 @@ package pages.studio;
 import com.microsoft.playwright.FrameLocator;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import java.util.ArrayList;
+import java.util.List;
 import utils.WaitUtility;
 
 public class BrandExplorerWorkspace {
@@ -12,16 +14,19 @@ public class BrandExplorerWorkspace {
     private final Locator BRAND_EXPLORER_CHART;
     private final Locator BRAND_EXPLORER_TABLE;
     private final Locator SAVE_WORKSPACE;
+    private final Locator DATE_RANGE_SELECTOR;
     WaitUtility waitUtility;
 
     public BrandExplorerWorkspace(Page page) {
         this.page = page;
         this.waitUtility = new WaitUtility(page);
-        this.WORKSPACE_FRAME = page.frameLocator("iframe").frameLocator("iframe");
+        this.WORKSPACE_FRAME = this.page.frameLocator("iframe").frameLocator("iframe");
         this.BRAND_EXPLORER_CHART = WORKSPACE_FRAME.locator("//div[@class='recharts-responsive-container']");
         this.BRAND_EXPLORER_TABLE = WORKSPACE_FRAME.locator("//div[contains(@class,'Box')]//table");
         this.SAVE_WORKSPACE = WORKSPACE_FRAME.locator(
                 "//button[contains(@data-tour-id,'save-workspace-button')]//div[contains(text(),'Save')]");
+        this.DATE_RANGE_SELECTOR = WORKSPACE_FRAME.locator(
+                "//p[normalize-space()='Time Frame']/following-sibling::div//input[starts-with(@id,'listbox-input-')]");
     }
 
     public void waitForDashboardLoad() {
@@ -53,5 +58,21 @@ public class BrandExplorerWorkspace {
     public void saveBrandExplorerWorkspace() {
         waitForDashboardLoad();
         SAVE_WORKSPACE.first().click();
+    }
+
+    public void clickTimeFrameSelector() {
+        waitUtility.waitForLocatorVisible(DATE_RANGE_SELECTOR);
+        DATE_RANGE_SELECTOR.click();
+    }
+
+    public List<String> getTimeFrameOptions() {
+        Locator options = WORKSPACE_FRAME.locator("//div[@role='dialog']//li[@role='option']");
+        waitUtility.waitForLocatorVisible(options.first());
+        List<String> optionlabels = new ArrayList<>();
+        int count = options.count();
+        for (int i = 0; i < count; i++) {
+            optionlabels.add(options.nth(i).innerText().trim());
+        }
+        return optionlabels;
     }
 }
